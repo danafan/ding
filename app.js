c449e8e67834b6398630e6c0738bedbcaafa13a4
范玉龙 <13067882143@163.com>
2019-01-14 17:22:33 +0800

    现在线上的版本

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 App({
   globalData: {
-    // baseurl: "http://erpcs.ppg8090.com/api/",        //正式
-    baseurl: "http://ceshi.ppg8090.com/api/",     //测试
+    baseurl: "http://erpcs.ppg8090.com/api/",        //正式
+    // baseurl: "http://ceshi.ppg8090.com/api/",     //测试
     username: "",                                    //用户名
     printer:"",                                      //选择的打印机
     codeObj: {}                                      //扫码获取的信息
